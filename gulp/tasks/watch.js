36b705e9e6cb1f8fce0e40e9faba2ddb3b1bdb83
Dustin Boersma <dustin.boersma@gmail.com>
2014-11-03 16:18:10 -0800

    moved setWatch functionality to watch command

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -6,6 +6,10 @@
 var gulp  = require('gulp');
 var config= require('../config');
 
+gulp.task('setWatch', function() {
+  global.isWatching = true;
+});
+
 gulp.task('watch', ['setWatch', 'browserSync'], function() {
   gulp.watch(config.sass.src,   ['sass']);
   gulp.watch(config.images.src, ['images']);
